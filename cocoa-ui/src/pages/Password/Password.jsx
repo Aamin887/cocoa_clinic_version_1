4492efb88d1a662994b6b4b8e884f141bdbd7779
Aamin887 <forkhamin@yahoo.co.uk>
2023-12-06 23:50:49 +0000

    adding password reset page

---
+++ cocoa-ui/src/pages/Password/Password.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './password.css'
+
+function Password() {
+  return (
+    <div className='password-page bg-dark'>
+        <div className="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eligendi facere exercitationem quae nostrum excepturi! Sit corrupti error, dicta eaque consequuntur optio, autem eveniet quidem omnis incidunt expedita magnam impedit.
+        </div>
+    </div>
+  )
+}
+
+export default Password
